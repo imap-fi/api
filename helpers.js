8c2b8f95d2fd96a542b26c38ae0d142470762e66
RoyTakanen <roy@imap.fi>
2022-03-08 16:10:17 +0200

    start and stop mysql connection inside auth func

--- helpers.js
+++ helpers.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql');
 const twinBcrypt = require('twin-bcrypt')
 
-const connection = mysql.createConnection({
-    host: process.env.MAILCOW_API_BASEURL.replace('https://', ''),
-    user: 'root',
-    password: process.env.DBROOT,
-    database: 'mailcow'
-});
-
-connection.connect();
-
 // Old auth: https://pastebin.fi/p/Ohby4U6uhNVQ
 exports.auth = async(email, pass) => {
+
+    const connection = mysql.createConnection({
+        host: process.env.MAILCOW_API_BASEURL.replace('https://', ''),
+        user: 'root',
+        password: process.env.DBROOT,
+        database: 'mailcow'
+    });
+
+    connection.connect();
+
     const data = await new Promise((resolve, reject) => {
         connection.query('SELECT username, password FROM mailbox WHERE username=?', [email + "@imap.fi"], (err, rows, fields) => {
             if (err)
@@ -19,6 +20,9 @@ exports.auth = async(email, pass) => {
             resolve(rows);
         });
     });
+
+    connection.end();
+
     if (data.length === 0)
         return false;
 
